ffc0ca806379d9375506d8fa51ed91a02ee465d4
Matea Cvetković <128927444+mateacvetkovic@users.noreply.github.com>
2025-12-21 18:15:51 +0000

    Integrate SnsLoggingService for error logging

--- src/main/java/COSN/exceptions/GlobalExceptionHandler.java
+++ src/main/java/COSN/exceptions/GlobalExceptionHandler.java
@@ -3,10 +3,11 @@ package COSN.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import COSN.service.SnsLoggingService;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,8 +15,15 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final SnsLoggingService loggingService;
+
+    public GlobalExceptionHandler(SnsLoggingService loggingService) {
+        this.loggingService = loggingService;
+    }
+
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public ResponseEntity<String> handleNotFound(EmailAlreadyExistsException ex) {
+        loggingService.publishError("Error", ex.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 
@@ -28,11 +36,12 @@ public class GlobalExceptionHandler {
         ex.getBindingResult().getFieldErrors().forEach(error ->
                 errors.put(error.getField(), error.getDefaultMessage())
         );
-
+        loggingService.publishError("Error", ex.getMessage());
         return ResponseEntity.badRequest().body(errors);
     }
     @ExceptionHandler(InvalidCredentialsException.class)
     public ResponseEntity<String> handleInvalidCredentials(InvalidCredentialsException ex) {
+        loggingService.publishError("Error", ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body(ex.getMessage());
@@ -40,19 +49,19 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
-        Map<String, String> error = new HashMap<>();
+        loggingService.publishError("Error", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(CarNotFoundException.class)
     public ResponseEntity<String> handleCarNotFound(CarNotFoundException ex) {
-        Map<String, String> error = new HashMap<>();
+        loggingService.publishError("Error", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler()
     public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
-        Map<String, String> error = new HashMap<>();
+        loggingService.publishError("Error", ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
     }
 
