6aff4c8784ff4c514f53cf3b37970c120aa217e4
Matea Cvetković <128927444+mateacvetkovic@users.noreply.github.com>
2025-12-21 18:17:41 +0000

    SNSLoggingService

---
+++ src/main/java/COSN/config/SnsConfig.java
@@ -0,0 +1,35 @@
+package COSN.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sns.SnsClient;
+
+@Configuration
+public class SnsConfig {
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Value("${aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public SnsClient snsClient() {
+        return SnsClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)
+                        )
+                )
+                .build();
+    }
+}
